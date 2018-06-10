0d8a85f67e26d40b1fe08614690466831b27d9ab
vitaminvp <vitamin@ukr.net>
2018-06-10 09:45:02 +0300

    some adding

--- wa_hw_3/js/script.js
+++ wa_hw_3/js/script.js
@@ -1,12 +1,26 @@
 $(function(){
 
-   function progress() {
-    bar = document.getElementById('bar');
-    bar.value += 1; // Увеличиваем значение на 2
-    if (bar.value < 100) {
-      var timer = setTimeout(progress, 500); // Вызываем функцию каждые 0.5 сек.
-    }
-   }
-
-    progress();
+    var timer;
+    $(".icon-Rounded-Rectangle-8").click(function(){
+        if($(this).hasClass("active")){
+            $(this).removeClass("active");
+            clearInterval(timer);
+        }else{
+            $(this).addClass("active");
+            var bar = $('#bar');
+            progress();
+        }
+    });
+    
+    
+    function progress() {
+        bar.value += 1; // Увеличиваем значение на 1
+            if (bar.value < 100) {
+                timer = setTimeout(progress, 50); // Вызываем функцию каждые 0.5 сек.
+            }else{
+               $(".icon-Rounded-Rectangle-8").removeClass("active"); 
+                bar.value = 0;
+            } 
+         }
+    
 });
