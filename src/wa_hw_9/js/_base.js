bf697d27f7547a2c551f08513c959e0fdafdebef
Vitamin <vitamin@ukr.net>
2018-06-15 16:47:59 +0300

    hw9

--- src/wa_hw_9/js/_base.js
+++ src/wa_hw_9/js/_base.js
@@ -79,7 +79,6 @@ function task3() {
 
 /*----------------------Задание 4----------------------------------------*/
 
-
 const btn4 = document.querySelector(".btn4");
 btn4.addEventListener("click", task4);
 function task4() {
@@ -100,4 +99,95 @@ function task4() {
         counter += salaries[key];
     }
     console.log("Cумму всех зарплат: " + counter);
+}
+/*----------------------Конец Задания 4----------------------------------------*/
+
+/*----------------------Задание 5---------------------------------------*/
+
+const btn5 = document.querySelector(".btn5");
+btn5.addEventListener("click", task5);
+function task5() {
+    console.clear();
+    const salaries5 = {
+        John: 400,
+        Jack: 20000,
+        vasya: 600,
+        Vitya: 39,
+        test: "«asdasdasd»"
+    }
+    let counter = 0;
+    let [maxUser, maxSalary] = [0, 0];
+    for (let key in salaries5) {
+        console.log(key +": "+ salaries5[key]);
+        if(isNaN(salaries5[key])){
+            continue;
+        }
+        if(maxSalary < salaries5[key])  [maxUser, maxSalary] = [key, salaries5[key]];
+        counter++;
+    }
+    counter > 0? console.log(`Максимальная зарплата у ${maxUser}: ${maxSalary}`):console.log("нет сотрудников");
+}
+
+/*----------------------Конец Задания 5----------------------------------------*/
+
+/*----------------------Задание 6---------------------------------------*/
+
+const btn6 = document.querySelector(".btn6");
+btn6.addEventListener("click", task6);
+function task6() {
+    console.clear();
+    const obj = {
+        John: 400,
+        Jack: 20000,
+        vasya: 600,
+        Vitya: 39,
+        test: "«asdasdasd»"
+    }
+    multiplyNumeric(obj);
+    for (let key in obj) {
+        console.log("x2:  " + key +": "+ obj[key]);
+    }
+}
+function multiplyNumeric(obj){
+    for (let key in obj) {
+        console.log(key +": "+ obj[key]);
+        if(isNaN(obj[key])){
+            continue;
+        }
+        obj[key]*=2;
+    }
+}
+
+/*----------------------Конец Задания 6----------------------------------------*/
+
+/*----------------------Задание 7---------------------------------------*/
+const btn7 = document.querySelector(".btn7");
+const arr7 = ["воз", "киборг", "корсет", "ЗОВ", "горбик", "костер", "сектор"];
+
+btn7.addEventListener("click", task7);
+
+function task7() {
+    console.clear();
+    console.log(arr7);
+    alert(`Случайный элемент: ${arr7[randomInteger(0, arr7.length-1)]}`);
+}
+
+/*----------------------Конец Задания 7----------------------------------------*/
+
+/*----------------------Задание 8---------------------------------------*/
+
+const btn8 = document.querySelector(".btn8");
+btn8.addEventListener("click", task8);
+const arr8 = [];
+function task8() {
+    console.clear();
+    let arr8El = prompt("Введите эллемент масива (число):", "35");
+    if(!isNaN(arr8El)){
+        arr8.push(arr8El);
+        task8();
+    } else{
+        // break;
+    }
+    console.log(arr8);
+
 }
