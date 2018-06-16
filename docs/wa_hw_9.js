0f7509b8c2fc4f1ac933cbeb2a2d7b9af820b972
Vitalii Vitaminenko <vitamin@ukr.net>
2018-06-16 15:49:20 +0300

    Second from home

--- docs/wa_hw_9.js
+++ docs/wa_hw_9.js
@@ -131,8 +131,8 @@ function integerOnly(e) {
     }
 }
 function randomInteger(min, max) {
-    var rand = min - .5 + Math.random() * (max + 1 - min);
-    rand = Math.floor(rand);
+    var rand = min - 0.5 + Math.random() * (max - min + 1);
+    rand = Math.round(rand);
     return rand;
 }
 /*----------------------Конец Задания 1----------------------------------------*/
@@ -199,6 +199,218 @@ function task4() {
     }
     console.log("Cумму всех зарплат: " + counter);
 }
+/*----------------------Конец Задания 4----------------------------------------*/
+
+/*----------------------Задание 5---------------------------------------*/
+
+var btn5 = document.querySelector(".btn5");
+btn5.addEventListener("click", task5);
+function task5() {
+    console.clear();
+    var salaries5 = {
+        John: 400,
+        Jack: 20000,
+        vasya: 600,
+        Vitya: 39,
+        test: "«asdasdasd»"
+    };
+    var counter = 0;
+    var maxUser = 0,
+        maxSalary = 0;
+
+    for (var key in salaries5) {
+        console.log(key + ": " + salaries5[key]);
+        if (isNaN(salaries5[key])) {
+            continue;
+        }
+        if (maxSalary < salaries5[key]) {
+            ;
+            var _ref = [key, salaries5[key]];
+            maxUser = _ref[0];
+            maxSalary = _ref[1];
+        }counter++;
+    }
+    counter > 0 ? console.log("\u041C\u0430\u043A\u0441\u0438\u043C\u0430\u043B\u044C\u043D\u0430\u044F \u0437\u0430\u0440\u043F\u043B\u0430\u0442\u0430 \u0443 " + maxUser + ": " + maxSalary) : console.log("нет сотрудников");
+}
+
+/*----------------------Конец Задания 5----------------------------------------*/
+
+/*----------------------Задание 6---------------------------------------*/
+
+var btn6 = document.querySelector(".btn6");
+btn6.addEventListener("click", task6);
+function task6() {
+    console.clear();
+    var obj = {
+        John: 400,
+        Jack: 20000,
+        vasya: 600,
+        Vitya: 39,
+        test: "«asdasdasd»"
+    };
+    multiplyNumeric(obj);
+    for (var key in obj) {
+        console.log("x2:  " + key + ": " + obj[key]);
+    }
+}
+function multiplyNumeric(obj) {
+    for (var key in obj) {
+        console.log(key + ": " + obj[key]);
+        if (isNaN(obj[key])) {
+            continue;
+        }
+        obj[key] *= 2;
+    }
+}
+
+/*----------------------Конец Задания 6----------------------------------------*/
+
+/*----------------------Задание 7---------------------------------------*/
+var btn7 = document.querySelector(".btn7");
+var arr7 = ["воз", "киборг", "корсет", "ЗОВ", "горбик", "костер", "сектор", 1, 99, 158];
+
+btn7.addEventListener("click", task7);
+
+function task7() {
+    console.clear();
+    console.log(arr7);
+    alert("\u0421\u043B\u0443\u0447\u0430\u0439\u043D\u044B\u0439 \u044D\u043B\u0435\u043C\u0435\u043D\u0442: " + arr7[randomInteger(0, arr7.length - 1)]);
+}
+
+/*----------------------Конец Задания 7----------------------------------------*/
+
+/*----------------------Задание 8---------------------------------------*/
+
+var btn8 = document.querySelector(".btn8");
+btn8.addEventListener("click", task8);
+var arr8 = [];
+function task8() {
+    console.clear();
+    var arr8El = prompt("Введите эллемент масива (число):", "35");
+    askAndPush(arr8, arr8El);
+    console.log(arr8);
+    alert(arr8);
+}
+function askAndPush(arr, element) {
+    if (!isNaN(element) && element !== null) {
+        arr.push(+element);
+        var arr8Element = prompt("Введите следующее значение:", "99");
+        askAndPush(arr, arr8Element);
+    }
+}
+
+/*----------------------Конец Задания 8----------------------------------------*/
+
+/*----------------------Задание 9---------------------------------------*/
+
+var btn9 = document.querySelector(".btn9");
+btn9.addEventListener("click", task9);
+function task9() {
+    var value = prompt('Введите значение эллемента масива', 'ЗОВ');
+    alert(find(arr7, value));
+}
+function find(arr, value) {
+    console.clear();
+    console.log(arr);
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] == value) {
+            console.log("\"" + arr[i] + "\" - \u044D\u043B\u043B\u0435\u043C\u0435\u043D\u0442 \u043C\u0430\u0441\u0441\u0438\u0432\u0430 \u2116 " + (i + 1) + ".");
+            return "\u042D\u043B\u043B\u0435\u043C\u0435\u043D\u0442 \u043C\u0430\u0441\u0441\u0438\u0432\u0430 \u2116 " + (i + 1) + ".";
+        }
+    }
+    return -1;
+}
+/*----------------------Конец Задания 9----------------------------------------*/
+
+/*----------------------Задание 10---------------------------------------*/
+var btn10 = document.querySelector(".btn10");
+btn10.addEventListener("click", task10);
+function task10() {
+    var arrNumrer = [1, 2, 3, 4, 5, 6, 8, 77, 99, 661, 888, 521478, "jjj", 54, 1, 22, 32, 7, 66, 555, 4, 5, 1000];
+    var a = prompt('Введите нижнюю границу диаппазона', '4');
+    var b = prompt('Введите верхнюю границу диаппазона', '600');
+    console.clear();
+    console.log("\u0418\u0441\u0445\u043E\u0434\u043D\u044B\u0439 \u043C\u0430\u0441\u0441\u0438\u0432: " + arrNumrer);
+    console.log("Новый массив: " + filterRange(arrNumrer, +a, +b));
+}
+function filterRange(arr, a, b) {
+    var newArr = [];
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] >= a && arr[i] <= b) {
+            newArr.push(arr[i]);
+        }
+    }
+    return newArr;
+}
+/*----------------------Конец Задания 10----------------------------------------*/
+
+/*----------------------Задание 11---------------------------------------*/
+var btn11 = document.querySelector(".btn11");
+btn11.addEventListener("click", task11);
+function task11() {
+    var x = prompt('Введите число', '4');
+    var n = prompt('Введите степень', '3');
+    console.clear();
+    if (!isNaN(x) && !isNaN(n)) {
+        console.log(pow(+x, +n));
+        alert(pow(+x, +n));
+    } else {
+        console.log("ERROR!");
+    }
+}
+function pow(a, b) {
+    if (b == 0) {
+        return 1;
+    } else if (b < 0) {
+        for (var i = 2; i <= Math.abs(b); i++) {
+            a *= a;
+        }
+        return 1 / a;
+    } else {
+        for (var _i = 2; _i <= b; _i++) {
+            a *= a;
+        }
+        return a;
+    }
+}
+/*----------------------Конец Задания 11----------------------------------------*/
+
+/*----------------------Задание 12---------------------------------------*/
+
+var btn12 = document.querySelector(".btn12");
+btn12.addEventListener("click", task12);
+function task12() {
+    console.clear();
+    var date = new Date(2012, 1, 20, 3, 12, 0, 0);
+    alert(date); // 20.02.2012, 03:12:00.000
+}
+
+/*----------------------Конец Задания 12----------------------------------------*/
+
+/*----------------------Задание 13---------------------------------------*/
+
+var btn13 = document.querySelector(".btn13");
+btn13.addEventListener("click", task13);
+function task13() {
+    var date = new Date();
+    getWeekDay(date);
+}
+
+function getWeekDay(date) {
+    var days = ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"];
+    console.clear();
+    var day = date.getDay();
+    console.log(days[day]);
+    alert(days[day]);
+}
+
+/*----------------------Конец Задания 13----------------------------------------*/
+
+/*----------------------Задание 14---------------------------------------*/
+
+/*----------------------Конец Задания 14----------------------------------------*/
+
+/*----------------------Задание 11---------------------------------------*/
 
 /***/ })
 
--- src/wa_hw_9/js/_base.js
+++ src/wa_hw_9/js/_base.js
@@ -31,8 +31,8 @@ function integerOnly(e){
     }
 }
 function randomInteger(min, max) {
-    var rand = min-.5 + Math.random() * (max + 1 - min);
-    rand = Math.floor(rand);
+    var rand = min - 0.5 + Math.random() * (max - min + 1)
+    rand = Math.round(rand);
     return rand;
 }
 /*----------------------Конец Задания 1----------------------------------------*/
@@ -162,7 +162,7 @@ function multiplyNumeric(obj){
 
 /*----------------------Задание 7---------------------------------------*/
 const btn7 = document.querySelector(".btn7");
-const arr7 = ["воз", "киборг", "корсет", "ЗОВ", "горбик", "костер", "сектор"];
+const arr7 = ["воз", "киборг", "корсет", "ЗОВ", "горбик", "костер", "сектор", 1, 99, 158];
 
 btn7.addEventListener("click", task7);
 
@@ -182,12 +182,131 @@ const arr8 = [];
 function task8() {
     console.clear();
     let arr8El = prompt("Введите эллемент масива (число):", "35");
-    if(!isNaN(arr8El)){
-        arr8.push(arr8El);
-        task8();
-    } else{
-        // break;
-    }
+    askAndPush(arr8, arr8El);
     console.log(arr8);
-
+    alert(arr8);
 }
+function askAndPush(arr, element){
+    if(!isNaN(element) && element!==null){
+        arr.push(+element);
+        let arr8Element = prompt("Введите следующее значение:", "99");
+        askAndPush(arr, arr8Element);
+    }
+}
+
+/*----------------------Конец Задания 8----------------------------------------*/
+
+/*----------------------Задание 9---------------------------------------*/
+
+const btn9 = document.querySelector(".btn9");
+btn9.addEventListener("click",  task9);
+function task9() {
+    let value = prompt('Введите значение эллемента масива', 'ЗОВ');
+    alert(find(arr7, value));
+}
+function  find(arr, value) {
+    console.clear();
+    console.log(arr);
+    for(let i=0; i < arr.length; i++){
+        if (arr[i] == value){
+            console.log(`"${arr[i]}" - эллемент массива № ${i + 1}.`)
+            return (`Эллемент массива № ${i+1}.`);
+        }
+    }
+    return -1;
+}
+/*----------------------Конец Задания 9----------------------------------------*/
+
+/*----------------------Задание 10---------------------------------------*/
+const btn10 = document.querySelector(".btn10");
+btn10.addEventListener("click",  task10);
+function task10() {
+    const arrNumrer = [1, 2, 3, 4, 5, 6, 8, 77, 99, 661, 888, 521478, "jjj", 54, 1, 22, 32, 7, 66, 555, 4, 5, 1000];
+    let a = prompt('Введите нижнюю границу диаппазона', '4');
+    let b = prompt('Введите верхнюю границу диаппазона', '600');
+    console.clear();
+    console.log(`Исходный массив: ${arrNumrer}`);
+    console.log("Новый массив: " + filterRange(arrNumrer, +a, +b));
+}
+function   filterRange(arr, a, b) {
+    const newArr = [];
+    for(let i=0; i < arr.length; i++){
+        if (arr[i] >= a && arr[i] <= b){
+            newArr.push(arr[i]);
+        }
+    }
+    return newArr;
+}
+/*----------------------Конец Задания 10----------------------------------------*/
+
+/*----------------------Задание 11---------------------------------------*/
+const btn11 = document.querySelector(".btn11");
+btn11.addEventListener("click",  task11);
+function task11() {
+    let x = prompt('Введите число', '4');
+    let n = prompt('Введите степень', '3');
+    console.clear();
+    if( !isNaN(x) && !isNaN(n) ){
+        console.log(pow(+x, +n));
+        alert(pow(+x, +n));
+    }else {
+        console.log("ERROR!");
+    }
+}
+function   pow(a, b) {
+    if( b == 0 ){
+        return 1;
+    }else if( b < 0 ){
+        for(let i=2; i <= Math.abs(b); i++){
+            a *= a;
+        }
+        return 1/a;
+    }
+    else{
+        for(let i=2; i <= b; i++){
+            a *= a;
+        }
+        return a;
+    }
+}
+/*----------------------Конец Задания 11----------------------------------------*/
+
+/*----------------------Задание 12---------------------------------------*/
+
+const btn12 = document.querySelector(".btn12");
+btn12.addEventListener("click",  task12);
+function task12() {
+    console.clear();
+    const date = new Date(2012, 1, 20, 3, 12, 0, 0);
+    alert( date ); // 20.02.2012, 03:12:00.000
+}
+
+
+/*----------------------Конец Задания 12----------------------------------------*/
+
+/*----------------------Задание 13---------------------------------------*/
+
+const btn13 = document.querySelector(".btn13");
+btn13.addEventListener("click",  task13);
+function task13() {
+    const date = new Date();
+    getWeekDay(date);
+}
+
+function getWeekDay(date){
+    const days = ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"];
+    console.clear();
+    let day = date.getDay();
+    console.log(days[day]);
+    alert( days[day] );
+}
+
+/*----------------------Конец Задания 13----------------------------------------*/
+
+/*----------------------Задание 14---------------------------------------*/
+
+
+
+/*----------------------Конец Задания 14----------------------------------------*/
+
+/*----------------------Задание 11---------------------------------------*/
