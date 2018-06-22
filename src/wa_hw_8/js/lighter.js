fac6b5d73c9bc812f4c8ed9685eebe553d10d39e
Vitamin <vitamin@ukr.net>
2018-06-22 17:53:20 +0300

    8

--- src/wa_hw_8/js/lighter.js
+++ src/wa_hw_8/js/lighter.js
@@ -3,4 +3,26 @@ import '../styles/lighter.scss'
 export function lighter() {
     const lighter = document.querySelector(".lighter");
     const lights = lighter.querySelectorAll(".lighter__light");
+    const ACTIVE = "lighter__light_active";
+    const lightsArray = Array.from(lights);
+    let index = 0;
+    lightsArray.forEach(function (item, i, lightsArray) {
+        item.addEventListener("click", function () {
+            turnOn(item, i);
+        });
+    })
+     function turnOn(item, i) {
+
+         console.log(item.className);
+         if(item.classList.contains(ACTIVE)){
+             turnOff();
+         }else{
+             item.classList.add(ACTIVE);
+         }
+         index = i;
+     }
+     function turnOff() {
+         lightsArray[index].classList.remove(ACTIVE);
+     }
 }
+lighter();
