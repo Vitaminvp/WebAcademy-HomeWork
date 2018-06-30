8139e12f7817cbc158b5d46631ffb187f3786985
Vitalii Vitaminenko <vitamin@ukr.net>
2018-06-30 23:45:21 +0300

    13

---
+++ src/wa_hw_13/wa_hw_13.js
@@ -0,0 +1,16 @@
+import './wa_hw_13.scss';
+import './js/_base.js';
+import { LightsManager } from './js/lights_manager';
+import { Lights } from './js/lights';
+
+
+
+const light = new LightsManager(document.querySelector('.wrapper'));
+
+
+const light1 = new Lights(document.querySelector('.wrapper'));
+// const light2 = new Lights(document.querySelector('.wrapper'));
+// const light3 = new Lights(document.querySelector('.wrapper'));
+// const light4 = new Lights(document.querySelector('.wrapper'));
+// const light5 = new Lights(document.querySelector('.wrapper'));
+
