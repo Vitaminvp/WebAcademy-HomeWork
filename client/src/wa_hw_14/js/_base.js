e489a94c0e715834a6b5efc687d0243408d1719f
Vitalii Vitaminenko <vitamin@ukr.net>
2018-07-05 06:30:39 +0300

    home

---
+++ client/src/wa_hw_14/js/_base.js
@@ -0,0 +1,39 @@
+const xhr = new XMLHttpRequest();
+let btn;
+let output;
+const wrapp = document.querySelector(".wrapper");
+(function render(){
+    btn = document.createElement('button');
+    btn.innerHTML = "GET DATA";
+    output  = document.createElement('div');
+    output.className = "output";
+    wrapp.appendChild(btn);
+    btn.addEventListener('click', function() {
+        wrapp.appendChild(output);
+        output.innerHTML = '<img src="https://vitaminvp.github.io/WA/client/assets/images/ajax-loader.gif">';
+        const timeout = 3000;
+        const timer = setTimeout(function () { xhr.abort(); alert("Зависон, однако!"); }, timeout);
+        xhr.open("GET", "http://localhost:4001/list");
+        xhr.onreadystatechange = function() {
+            if(xhr.readyState == 4 && xhr.status == 200){
+                output.innerHTML = "";
+                let data = JSON.parse(xhr.responseText);
+
+                data = Array.from(data);
+                data.forEach( (item) => {
+                    let content = "";
+                    content += "<ul>";
+                    for (let key in item){
+                        content += '<li>'+key+': "'+item[key]+'"</li>';
+                    }
+                    content += "</ul>";
+                    output.innerHTML += content;
+                    clearTimeout(timer);
+                });
+            }
+        };
+        xhr.send();
+    });
+})();
+
+
--- server/models/list.js
+++ server/models/list.js
@@ -6,57 +6,87 @@
  */
 var _ = require('lodash');
 var items = [
-  {
-    completed: false,
-    title: "Test",
-    id: Date.now()
-  },
-  {
-    completed: true,
-    title: "Hello",
-    id: Date.now() + 5
-  }
+    {
+        completed: false,
+        title: "Test",
+        id: Date.now()
+    },
+    {
+        completed: true,
+        title: "Hello",
+        id: Date.now() + 5
+    },
+    {
+        completed: true,
+        title: "Mello",
+        id: Date.now() + 15
+    },
+    {
+        completed: true,
+        title: "Nello",
+        id: Date.now() + 25
+    },
+    {
+        completed: true,
+        title: "Nello",
+        id: Date.now() + 35
+    },
+    {
+        completed: true,
+        title: "Nello",
+        id: Date.now() + 45
+    },
+    {
+        completed: true,
+        title: "Nello",
+        id: Date.now() + 55
+    },
+    {
+        completed: true,
+        title: "Nello",
+        id: Date.now() + 65
+    }
 ];
 
 var listModel = {
-  setItem: function (data) {
-    data.id = Date.now();
-    data.completed = false;
-    items.push(data);
-    console.log(data);
-    return data;
-  },
-  getItems: function () {
-    return items;
-  },
-  updateItem: function (data, id) {
-    var index = items.reduce((index, item, i) => {
-      if (Number(item.id) === Number(id)) {
-        index = i;
-      }
-      ;
-      return index
-    }, null);
-    if (!index && index !== 0) {
-      return index;
+    setItem: function (data) {
+        data.id = Date.now();
+        data.completed = false;
+        items.push(data);
+        console.log(data);
+        return data;
+    },
+    getItems: function () {
+        return items;
+    },
+    updateItem: function (data, id) {
+        var index = items.reduce((index, item, i) => {
+            if (Number(item.id) === Number(id)) {
+                index = i;
+            }
+            ;
+            return index
+        }, null);
+        if (!index && index !== 0) {
+            return index;
+        }
+
+        items[index].completed = data.completed;
+        items[index].title = data.title;
+
+        return items[index];
+    },
+    deleteItem: function (id) {
+        var index = items.indexOf(_.find(items, function (item) {
+            return item.id === Number(id);
+        }));
+
+        if (index === -1) {
+            return false;
+        }
+
+        return items.splice(index, 1);
     }
-
-    items[index].completed = data.completed;
-    items[index].title = data.title;
-
-    return items[index];
-  },
-  deleteItem: function (id) {
-    var index = items.indexOf(_.find(items, function (item) {
-      return item.id === Number(id);
-    }));
-
-    if (index === -1) {
-      return false;
-    }
-
-    return items.splice(index, 1);
-  }
 };
 
 module.exports = listModel;
