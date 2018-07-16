b08ae1755164db770f79695fe9f724914d3ab9df
Vitalii Vitaminenko <vitamin@ukr.net>
2018-07-16 08:35:45 +0300

    16

--- client/docs/wa_final_project.js
+++ client/docs/wa_final_project.js
@@ -98,6 +98,19 @@ __webpack_require__(443);
 "use strict";
 
 
+var toggle = document.querySelector('.toggle-mnu');
+var menuList = document.querySelector('.header__nav_menu');
+toggle.addEventListener('click', function (e) {
+    if (!toggle.classList.contains('on')) {
+        toggle.classList.add('on');
+        menuList.style.display = 'flex';
+    } else {
+        toggle.classList.remove('on');
+        menuList.style.display = 'none';
+    }
+    e.preventDefault();
+});
+
 /***/ })
 
 /******/ });
