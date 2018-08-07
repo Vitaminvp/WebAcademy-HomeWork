87548c86ad184bbe501436bf98a14425ab60ff15
Vitalii Vitaminenko <vitamin@ukr.net>
2018-08-07 20:33:34 +0300

    card home

--- client/src/cards/components/card/card.jsx
+++ client/src/cards/components/card/card.jsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
+import { Ajax } from './../../utils/ajax';
+const URL = 'https://ec-test-react.herokuapp.com/';
 
 const generateId = () => {
     return '_'+Math.random().toString(36).substr(2, 9);
-}
+};
 
 export class MyCard extends React.Component {
     constructor(){
@@ -11,8 +13,16 @@ export class MyCard extends React.Component {
 
 
     render(){
+        let imgStyle = {
+            width: "100px",
+            height: "100px"
+        };
+        Ajax.get(`${URL}api/v1/items`, (response) => {
+            imgStyle = Object.assign({}, response);
+             console.log(imgStyle);
+        });
         const pictures = this.props.imgsrc.map((item)=>{
-            return <img key = {generateId()} src={'./../assets/' + item} alt=""/>;
+            return <img key = {generateId()} src={'./../assets/' + item} style={ imgStyle } alt=""/>;
         });
         return <div>{pictures}</div>;
     }
